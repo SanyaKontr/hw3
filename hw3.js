dd9cb1d250f4b25bbfb4309bf9685e2336b63ef8
SanyaKontr <Bs13111@mail.ru>
2023-08-19 02:06:50 +0300

    first commit

---
+++ hw3.js
@@ -0,0 +1,124 @@
+/*let a=20;
+alert(a);
+
+let name = 'Брендан Эйх';
+alert(name);
+
+let x = 10;
+let y = 2;
+t=x+y;
+i=x-y
+c=x*y;
+d=x/y;
+alert(t+'\n'+i+'\n'+c+'\n'+d);
+
+let result = 2**5;
+alert(result);
+
+let a=9;
+let b=2;
+alert(a%b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age=prompt('Сколько вам лет?');
+alert('Вам ' + age + ' лет')
+
+const user = {
+    name: 'John',
+    age: 37,
+    isAdmin: true,
+   
+}
+user['age'] = 25;
+
+/*user['city of residence'];
+
+let info=prompt('Какую информацию хотите узнать о пользователе?')
+
+alert(user[info]);
+
+let nameUser= prompt('Ваше имя')
+alert('Привет, ' + nameUser+'!')*/
+
+/*let password = '12345';
+let passwordUser = prompt('Введите пароль');
+
+if (password === passwordUser) {
+   alert('Пароль введен верно') 
+} else {
+    alert('Пароль введен не верно') 
+}
+
+let c = 7;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Не верно');
+}
+
+let d = 150;
+let e = 25;
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Не верно');
+}
+
+
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(Number(a) + Number(b));
+
+
+let month = String(prompt('Какой сейчас номер месяца?'))
+switch (month) {
+    case '1':
+        console.log('Зима');
+        break;
+    case '2':
+        console.log('Зима');
+        break;
+    case '3':
+        console.log('Весна');
+        break;
+    case '4':
+        console.log('Весна');
+        break;
+    case '5':
+        console.log('Весна');;
+        break;
+    case '6':
+        console.log('Лето');
+        break;
+    case '7':
+        console.log('Лето');
+        break;
+    case '8':
+        console.log('Лето');
+        break;
+    case '9':
+        console.log('Осень');
+        break;
+    case '10':
+        console.log('Осень');
+        break;
+    case '11':
+        console.log('Осень');
+        break;
+    case '12':
+        console.log('Зима');
+        break;
+    default:
+        console.log('Такого месяца не существует');
+        break;
+}*/
