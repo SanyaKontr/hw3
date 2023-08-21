327a13b0b2ff91645087ae523026f3591c551f23
SanyaKontr <Bs13111@mail.ru>
2023-08-21 19:33:59 +0300

    исправление дз

--- hw4.js
+++ hw4.js
@@ -1,53 +1,42 @@
+//1 задание
 let a=1;
 let b = 2;
 while (a<=b) {
-    console.log('Привет');
-    a++;
+   console.log('Привет');
+   a++;
 }
 
+//2 задание
 let x=1;
 let y = 5;
 while (x<=y) {
-    console.log(x);
-       x++;
+   console.log(x);
+   x++;
 }
 
+//3 задание
 let t = 7;
 let g = 22;
 while (t<=g) {
-    console.log(t);
-       t++;
+   console.log(t);
+   t++;
 }
 
-let obj = { Коля: 200, Вася: 300, Петя: 400 };
+//4 задание
+let obj = { "Коля": 200, "Вася": 300, "Петя": 400 };
 for (let money in obj){
    console.log(`${money} зарплата ${obj[money]} долларов`);
 }
 
+//5 задание
 let n = 1000;
 let num=0;
 while (n>=50)
-{
-n=n/2;
-num++;
-}
+      {n=n/2;
+      num++;}
 alert (num);
 
-
-let fri = 0;
-while (fri!==26) {; 
-	fri++
-
-	if (fri === 5) {
-		console.log(`Сегодня пятница, ${fri}-е число. Необходимо подготовить отчет.`);
-       fri+7;}
-    if (fri === 12) {
-		console.log(`Сегодня пятница, ${fri}-е число. Необходимо подготовить отчет.`);
-       fri+7;}
-    if (fri === 19) {
-		console.log(`Сегодня пятница, ${fri}-е число. Необходимо подготовить отчет.`);
-       fri+7;}
-    if (fri === 26) {
-		console.log(`Сегодня пятница, ${fri}-е число. Необходимо подготовить отчет.`);
-       fri+7;}
+//6 задание
+for (let friday = 5; friday <= 31; friday +=7){
+   console.log(`Сегодня пятница ${friday}-е число. Необходимо подготовить отчет`);
 }
